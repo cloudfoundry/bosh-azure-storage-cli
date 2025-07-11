335e37dd260bb27e115dbb15a052114635d983bc
D059372 <johannes.haass@sap.com>
2025-07-11 11:41:30 +0200

    return empty json when blob does not exist

--- client/storage_client.go
+++ client/storage_client.go
@@ -346,7 +346,6 @@ type BlobProperties struct {
 func (dsc DefaultStorageClient) Properties(
 	dest string,
 ) error {
-
 	blobURL := fmt.Sprintf("%s/%s", dsc.serviceURL, dest)
 
 	log.Println(fmt.Sprintf("Getting properties for blob %s", blobURL)) //nolint:staticcheck
@@ -357,6 +356,10 @@ func (dsc DefaultStorageClient) Properties(
 
 	resp, err := client.GetProperties(context.Background(), nil)
 	if err != nil {
+		if strings.Contains(err.Error(), "RESPONSE 404") {
+			fmt.Println(`{}`)
+			return nil
+		}
 		return fmt.Errorf("failed to get properties for blob %s: %w", dest, err)
 	}
 
